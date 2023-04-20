71aa87b28c03802620a8fe00d2d3372d6573e893
remi-h <remyenar@yahoo.com>
2023-04-20 16:40:20 +0900

    ' -> &apos;

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,13 +13,13 @@ export default function About() {
                 </p>
                 <h2>ABOUT ME</h2>
                 <p className="about">
-                    Hi! I'm Remi from Japan, and I made this website and let me tell you my story. <br/>
+                    Hi! I&apos;m Remi from Japan, and I made this website and let me tell you my story. <br/>
                     During the summer of 2021, I flied to Sweden to do my exchange studies at KTH Royal Institute of Technology in Stockholm.<br/>
                     It was my first time visiting Europe and I fell in live with the city and I immediately knew I wanted to come back. <br />
-                    Now I came back to Japan but luckily, I got a job in Stockholm and I'm going back there. Then, I realized, this time it's not just a year living abroad.<br />
-                    I don't know when I'll come back to Japan next time and I will miss people here. So, I made this website so that I always know if it's a good time to call to my friends and family in Japan.<br />
-                    AND, getting to know time is not enough, I need to know the weather, if it's holiday, etc...<br />
-                    So, here it is! I'm still adding some features but it's coming. Let me know if you have any idea too! Thank you for reading this long :)
+                    Now I came back to Japan but luckily, I got a job in Stockholm and I&apos;m going back there. Then, I realized, this time it&apos;s not just a year living abroad.<br />
+                    I don&apos;t know when I&apos;ll come back to Japan next time and I will miss people here. So, I made this website so that I always know if it&apos;s a good time to call to my friends and family in Japan.<br />
+                    AND, getting to know time is not enough, I need to know the weather, if it&apos;s holiday, etc...<br />
+                    So, here it is! I&apos;m still adding some features but it&apos;s coming. Let me know if you have any idea too! Thank you for reading this long :)
                 </p>
             </section>
         </>
